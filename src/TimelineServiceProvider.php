93bf8311a4b23ac2e235ae263b22dedd19764d9d
cesarbosqueiro <76708535+cesarbosqueiro@users.noreply.github.com>
2025-06-10 17:09:45 +0000

    Fix styling

--- src/TimelineServiceProvider.php
+++ src/TimelineServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Bosqro\Timeline;
 
+use Bosqro\Timeline\Commands\TimelineCommand;
+use Bosqro\Timeline\Testing\TestsTimeline;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Bosqro\Timeline\Commands\TimelineCommand;
-use Bosqro\Timeline\Testing\TestsTimeline;
 
 class TimelineServiceProvider extends PackageServiceProvider
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace Bosqro\Timeline\Tests;
 
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
+use Bosqro\Timeline\TimelineServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
 use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
@@ -16,7 +17,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Bosqro\Timeline\TimelineServiceProvider;
 
 class TestCase extends Orchestra
 {
